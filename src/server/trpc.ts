17ed1a76d39516b16d0bdfa32f0d53e0bd78b629
guhzoide <hugorobertiferreira61.br@gmail.com>
2026-05-13 13:59:59 -0300

    Migrando para mongo db

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,14 +1,8 @@
 import { initTRPC, TRPCError } from '@trpc/server';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 // ─── Prisma ───────────────────────────────────────────────────────────────────
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
 
 // ─── Context ──────────────────────────────────────────────────────────────────
 export const createContext = async (opts?: any) => {
