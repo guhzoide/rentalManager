cb098c541fb6c16b469530c34be24624a767eb67
guhzoide <hugorobertiferreira61.br@gmail.com>
2026-05-21 12:10:28 -0300

    fix: ajuste versel

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,6 +2,15 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db.js";
 
+const origins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+
+if (process.env.BETTER_AUTH_URL) {
+    origins.push(process.env.BETTER_AUTH_URL);
+}
+if (process.env.VERCEL_URL) {
+    origins.push(`https://${process.env.VERCEL_URL}`);
+}
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "mongodb",
@@ -9,6 +18,6 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
-    trustedOrigins: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    trustedOrigins: origins,
 });
 export type Auth = typeof auth;
